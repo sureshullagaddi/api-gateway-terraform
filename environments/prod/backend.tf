f50c9894007ee8db55812545275baa0a1b60b9f7
sureshullagaddi <33369988+sureshullagaddi@users.noreply.github.com>
2026-05-29 15:29:38 +0200

    added tf-state

--- environments/prod/backend.tf
+++ environments/prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "tf-state-demo-bucket"
+    bucket         = "tf-state-397979615352"
     key            = "prod/terraform.tfstate"
     region         = "eu-north-1"
     dynamodb_table = "terraform-lock"
