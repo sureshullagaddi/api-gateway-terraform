c2896f7ddb66059b69b2aa4fcd2ddff856c13f08
sureshullagaddi <33369988+sureshullagaddi@users.noreply.github.com>
2026-06-02 11:49:10 +0200

    removed self-service from github actions

--- environments/api-factory/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    # bucket, key, region, dynamodb_table passed via -backend-config in workflow
-    # key = "api-factory/<api_name>/terraform.tfstate"  (set dynamically per API)
-    encrypt = true
-  }
-}
-
